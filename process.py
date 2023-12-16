ff92c77aef10e109fdfe07e6bd8aad83d8a897ac
Avdyl Krasniqi <iamavdylk@gmail.com>
2023-12-17 00:34:47 +0100

    group journeys based on activity & filter based on time threshold, then add a processing script

---
+++ process.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+csv_file_path = '1-safe.csv'
+
+df = pd.read_csv(csv_file_path)
+
+grouped_display_names = df.groupby('GroupID')['Display Name'].unique()
+
+
+# Iterating over each group and printing the formatted output with spaces replaced by underscores
+for group_id, names in grouped_display_names.items():
+    names_with_underscores = [name.replace(" ", "_") for name in names]
+    formatted_output = f"{len(names)} {' '.join(names_with_underscores)}"
+    print(formatted_output)
